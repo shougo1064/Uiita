41ba5f687dbe5289860d28ca906c97378920f8e0
依田尚悟 <>
2021-06-30 08:34:48 +0900

    記事の model spec を修正

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,6 +4,7 @@
 #
 #  id         :bigint           not null, primary key
 #  body       :text
+#  status     :string           default("draft")
 #  title      :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
@@ -19,39 +20,54 @@
 #
 require "rails_helper"
 
+require "rails_helper"
+
 RSpec.describe Article, type: :model do
-  context "記事の title, body が指定されているとき" do
-    let(:user) { build(:user) }
-    let(:article) { build(:article, user: user) }
-    it "記事が投稿できる" do
+  context "記事のタイトルと本文が入力されているとき" do
+    let(:article) { build(:article) }
+    it "下書き状態の記事が作成できる" do
       expect(article).to be_valid
+      expect(article[:status]).to eq "draft"
     end
   end
 
-  context "title が指定されていない時" do
-    let(:user) { build(:user) }
+  context "status が下書き状態のとき" do
+    let(:article) { build(:article, :draft) }
+    it "記事を下書き状態で作成できる" do
+      expect(article).to be_valid
+      expect(article[:status]).to eq "draft"
+    end
+  end
+
+  context "status が公開状態のとき" do
+    let(:article) { build(:article, :published) }
+    it "記事を公開状態で作成できる" do
+      expect(article).to be_valid
+      expect(article[:status]).to eq "published"
+    end
+  end
+
+  context "記事の title が指定されていないとき" do
     let(:article) { build(:article, title: nil) }
-    it "記事の投稿に失敗する" do
+    it "記事が投稿できない" do
       expect(article).to be_invalid
-      expect(article.errors.details[:title][0][:error]).to eq :blank
+      expect(article.errors.messages[:title]).to eq ["can't be blank"]
     end
   end
 
-  context "body が指定されていない時" do
-    let(:user) { build(:user) }
+  context "記事の body が指定されていないとき" do
     let(:article) { build(:article, body: nil) }
-    it "記事の投稿に失敗する" do
+    it "記事が投稿できない" do
       expect(article).to be_invalid
-      expect(article.errors.details[:body][0][:error]).to eq :blank
+      expect(article.errors.messages[:body]).to eq ["can't be blank"]
     end
   end
 
-  context "title の文字数が51文字以上の時" do
-    let(:user) { build(:user) }
+  context "記事の title の最大文字数が51文字以上のとき" do
     let(:article) { build(:article, title: "a" * 51) }
-    it "記事の投稿に失敗する" do
+    it "記事が投稿できない" do
       expect(article).to be_invalid
-      expect(article.errors.details[:title][0][:error]).to eq :too_long
+      expect(article.errors.messages[:title]).to eq ["is too long (maximum is 50 characters)"]
     end
   end
 end
