f0ffecd1b2e9c2193a903689c8936bf689b26a1a
依田尚悟 <>
2021-06-30 08:33:21 +0900

    enum を使って、記事の状態に関する記述を追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,7 @@
 #
 #  id         :bigint           not null, primary key
 #  body       :text
+#  status     :string           default("draft")
 #  title      :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
@@ -24,4 +25,6 @@ class Article < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 50 }
   validates :body, presence: true
+
+  enum status: { draft: "draft", published: "published" }
 end
