d53bb872fe80c24870834c86360efafc249feec0
依田尚悟 <>
2021-07-04 17:51:29 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,23 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :articles do
+        collection do
+          get "search"
+        end
+        resources :drafts, only: [:index, :show]
+      end
+
       resources :article_likes, only: [:create, :destroy]
 
       mount_devise_token_auth_for "User", at: "auth", controllers: {
         registrations: "api/v1/auth/registrations",
       }
       resources :comments, only: [:create]
-      namespace :articles do
-        resources :drafts, only: [:index, :show]
-      end
 
       namespace :current do
         resources :articles, only: [:index]
       end
-      resources :articles
     end
   end
 end
