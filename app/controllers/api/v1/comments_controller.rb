2f1fc73297ac12424a97cbfd5a21c90c29c15e2b
依田尚悟 <>
2021-07-08 13:58:04 +0900

    記事のコメント一覧の API を実装

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,8 +1,14 @@
 module Api::V1
   class CommentsController < BaseApiController
     before_action :authenticate_user!, only: [:create]
+    def index
+      article = Article.published.find(params["article"]["id"])
+      comments = article.comments.order(created_at: :desc)
+      render json: comments, each_serializer: Api::V1::CommentSerializer
+    end
+
     def create
-      Article.find(params["comment"]["article_id"])
+      Article.published.find(params["comment"]["article_id"])
       comment = current_user.comments.create!(comment_params)
       render json: comment, serializer: Api::V1::CommentSerializer
     end
