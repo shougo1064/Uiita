95c8af02b3f5fff1cec06bed318b70ed0ef98963
依田尚悟 <>
2021-07-09 11:30:15 +0900

    comment シリアライザーの修正

--- app/serializers/api/v1/comment_serializer.rb
+++ app/serializers/api/v1/comment_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::CommentSerializer < ActiveModel::Serializer
-  attributes :id, :body
+  attributes :id, :body, :created_at
   belongs_to :article, serializer: Api::V1::ArticleSerializer
   belongs_to :user, serializer: Api::V1::UserSerializer
 end
