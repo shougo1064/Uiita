8e2b6f85a5a49405b78021efbc3716cc1841ed0f
依田尚悟 <>
2021-07-09 11:30:39 +0900

    記事の一覧を取得する API の内容を修正

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -2,13 +2,13 @@ module Api::V1
   class CommentsController < BaseApiController
     before_action :authenticate_user!, only: [:create]
     def index
-      article = Article.find(params["article"]["id"])
+      article = Article.published.find(params["comment"]["article_id"])
       comments = article.comments.order(created_at: :desc)
       render json: comments, each_serializer: Api::V1::CommentSerializer
     end
 
     def create
-      Article.find(params["comment"]["article_id"])
+      Article.published.find(params["comment"]["article_id"])
       comment = current_user.comments.create!(comment_params)
       render json: comment, serializer: Api::V1::CommentSerializer
     end
@@ -16,7 +16,7 @@ module Api::V1
     private
 
       def comment_params
-        params.require(:comment).permit(:body, :article_id)
+        params.require(:comment).permit(:body, :article_id, :created_at)
       end
   end
 end
