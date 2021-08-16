593c632ba13c47143c90c3530c35a0de119736ae
依田尚悟 <>
2021-08-16 11:05:56 +0900

    リロード時にエラーが出ないように調整

--- front/store/article.js
+++ front/store/article.js
@@ -1,14 +1,19 @@
 export const state = () => ({
   articles: [],
+  article: {},
 })
 
 export const getters = {
   articles: (state) => state.articles,
+  article: (state) => state.article,
 }
 
 export const mutations = {
   setArticles(state, articles) {
     state.articles = articles
+  },
+  setArticle(state, article) {
+    state.article = article
   },
 }
 
@@ -18,6 +23,14 @@ export const actions = {
       const data = response.data
 
       commit('setArticles', data)
+    })
+  },
+
+  async fetchArticle({ commit }, id) {
+    await this.$axios.get(`api/v1/articles/${id}`).then((response) => {
+      const data = response.data
+
+      commit('setArticle', data)
     })
   },
   async createArticle(_, params) {
