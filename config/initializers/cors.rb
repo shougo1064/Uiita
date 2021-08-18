6079ade3f7b39dda787b54eb79ec8d8ea7f6498e
依田尚悟 <>
2021-08-18 11:42:47 +0900

    各種URLに関する設定を変更

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins Settings.frontend.url
+    origins "http://localhost:8080"
     resource "*",
              headers: :any,
              expose: ["access-token", "expiry", "token-type", "uid", "client"],
